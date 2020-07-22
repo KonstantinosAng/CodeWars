4ee4e76601ea8a5ea41a608196d907cf13bfc7c5
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-07-22 18:23:21 +0300

    :zap: update testunit wrapper

--- Python/TestFunction.py
+++ Python/TestFunction.py
@@ -1,30 +1,40 @@
 from functools import wraps
 
-def test_it(func, parameters, test_case):
-  """
-  Test any function with test units
-  :param func: any python function
-  :return: None
-  """
-  @wraps(func)
-  def test(*args, **kwargs):
+
+class Test:
+
+  def __init__(self, tests):
+    self.test_cases = tests
+    self.result = None
+
+  def assert_result(self, result):
+    print("Test Case resulted in {} {}". format(result, "✅" if result==self.test_case else "❌"))
+
+
+def Tester(test_case):
+
+  def test_it(func):
     """
-    execute function and test it
-    :param args: any argument
-    :param kwargs: any keyword argument
+    Test any function with test units
+    :param func: any python function
     :return: None
     """
-    result = None
-    try:
-      result = func(*args, **kwargs) == test_case
-      return result
-    except Exception as e:
-      print("[TEST CASE: ({.__name__}) ] {} ❗❗❗".format(func, e))
-    finally:
-      if result is not None:
-        print("[FUNCTION] {.__name__} ➡ [TEST CASE] {}".format(func, test_case))
-        if result:
-          print("Test Case resulted in {} ✅". format(result))
-        else:
-          print("Test Case resulted in {} ❌". format(result))
-  return test(parameters)
+    @wraps(func)
+    def test(*args, **kwargs):
+      """
+      execute function and test it
+      :param args: any argument
+      :param kwargs: any keyword argument
+      :return: None
+      """
+      try:
+        result = func(*args, **kwargs) == test_case
+        return result
+      except Exception as e:
+        print("[TEST CASE: ({.__name__}) ] {} ❗❗❗".format(func, e))
+      finally:
+        if result is not None:
+          print("[FUNCTION] {.__name__} ➡ [TEST CASE] {}".format(func, test_case))
+          print("Test Case resulted in {} {}". format(result, "✅" if result else "❌"))
+    return test
+  return test_it
