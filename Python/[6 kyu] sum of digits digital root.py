670667acac391c872f0d13f6623d8e944c5ef1d9
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-04 14:18:37 +0300

    :sparkles: added new kata

--- Python/[6 kyu] sum of digits digital root.py
+++ Python/[6 kyu] sum of digits digital root.py
@@ -1,29 +1,16 @@
-# see https://www.codewars.com/kata/52dffa05467ee54b93000712/solutions/pythons
+# see https://www.codewars.com/kata/541c8630095125aba6000c00/solutions/python
 
 from TestFunction import Test
 
-def pseudo_sort(st):
-  punctuations = [',', '.', '!', ':', '?', ';']
-  for punc in punctuations:
-    st = st.replace(punc, "") 
-  if st.isspace(): return '' 
-  words = st.split(" ")
-  lowercase = []
-  uppercase = []
-  for word in words:
-    if word.lower() == word:
-      lowercase.append(word)
-    else:
-      uppercase.append(word)
-  lowercase = ' '.join([x for x in sorted(lowercase)])
-  uppercase = ' '.join([x for x in reversed(sorted(uppercase))])
-  if lowercase == '': return uppercase
-  if uppercase == '': return lowercase
-  return ' '.join([lowercase, uppercase])
+def digital_root(n):
+  rv = str(n)
+  while len(rv) != 1:
+    rv = str(sum(int(x) for x in str(rv)))
+  return int(rv)
 
-test = Test(None)
-test.assert_equals(pseudo_sort("I, habitan of the Alleghanies, treating of him as he is in himself in his own rights"), "as habitan he him himself his in in is of of own rights the treating I Alleghanies")
-test.assert_equals(pseudo_sort("take up the task eternal, and the burden and the lesson"), "and and burden eternal lesson take task the the the up")
-test.assert_equals(pseudo_sort("Land of the Old Thirteen! Massachusetts land! land of Vermont and Connecticut!"), "and land land of of the Vermont Thirteen Old Massachusetts Land Connecticut")
-test.assert_equals(pseudo_sort("Pioneers, O Pioneers!"), "Pioneers Pioneers O")
-test.assert_equals(pseudo_sort("    "), "")
+
+Test = Test(None)
+Test.assert_equals(digital_root(16), 7)
+Test.assert_equals(digital_root(942), 6)
+Test.assert_equals(digital_root(132189), 6)
+Test.assert_equals(digital_root(493193), 2)
