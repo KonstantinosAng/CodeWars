490defb751528239014ef897e8ebf8583cb76521
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-05 02:26:46 +0300

    :sparkles: added new kata

--- Python/[6 kyu] array diff.py
+++ Python/[6 kyu] array diff.py
@@ -1,8 +1,24 @@
-# see 
+# see https://www.codewars.com/kata/523f5d21c841566fde000009/solutions/python
 
 from TestFunction import Test
 
+def not_equal(x1, b):
+  for x2 in b:
+    if x1 == x2: return False
+  return True
 
+def array_diff(a, b):
+  if a == []: return []
+  if b == []: return a
+  rv = []
+  for x in a:
+    if not_equal(x, b):
+      rv.append(x)
+  return rv
 
 Test = Test(None)
-
+Test.assert_equals(array_diff([1,2], [1]), [2])
+Test.assert_equals(array_diff([1,2,2], [1]), [2,2])
+Test.assert_equals(array_diff([1,2,2], [2]), [1])
+Test.assert_equals(array_diff([1,2,2], []), [1,2,2])
+Test.assert_equals(array_diff([], [1,2]), [], "a was [], b was [1,2], expected []")
