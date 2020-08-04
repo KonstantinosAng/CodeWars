5acbc5c27248e039b43a674cd90d5dc31919c766
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-05 00:59:52 +0300

    :sparkles: added new kata

--- Python/[6 kyu] break camelCase.py
+++ Python/[6 kyu] break camelCase.py
@@ -1,12 +1,12 @@
-# see 
+# see https://www.codewars.com/kata/5208f99aee097e6552000148/solutions/python
 
 from TestFunction import Test
 
 
 def solution(s):
-  uppercase = [i for i, x in enumerate(s) if x.isupper()] 
-  return ' '.join([s[:i] for i in uppercase])
-    
+  indexes = [0] + [i for i, x in enumerate(s) if x.isupper()] 
+  return ' '.join([s[indexes[i]:indexes[i+1]] for i in range(len(indexes) - 1)]) + ' ' + s[indexes[-1]:]
+  
 
 Test = Test(None)
 Test.assert_equals(solution("helloWorld"), "hello World")
