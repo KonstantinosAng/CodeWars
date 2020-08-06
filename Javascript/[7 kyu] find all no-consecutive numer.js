8c9ce7d280744d508685f8bcbb08e1b98b1bc4a1
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-06 16:08:33 +0300

    :sparkles: added new kata

---
+++ Javascript/[7 kyu] find all no-consecutive numer.js
@@ -0,0 +1,15 @@
+// see 
+
+function allNonConsecutive (arr) {
+  if (arr.length === 1) { return [];}
+  let rv = []; 
+  for (j=0;j<arr.length-1;j++) {
+   if (arr[j+1] - arr[j] != 1) {
+    rv.push({i: j+1, n: arr[j+1]});
+   }
+ }
+ return rv
+}
+
+console.log(allNonConsecutive([1,2,3,4,6,7,8,10]), [{i: 4, n:6}, {i: 7, n:10}])
+console.log(allNonConsecutive([-5,-3,-2,0,1,2,3,4,6,7,8,10 ]), [{ i: 1, n: -3 }, { i: 3, n: 0 }, { i: 8, n: 6 }, { i: 11, n: 10 }])
