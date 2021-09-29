642c21c01c48a6eb2d3f67d4c110c1ae16473eed
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-09-29 16:48:56 +0300

    :sparkles: added new kata

--- Python/[7 kyu] even or odd - which is greater.py
+++ Python/[7 kyu] even or odd - which is greater.py
@@ -1,19 +1,20 @@
-# see https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
+# see https://www.codewars.com/kata/57f7b8271e3d9283300000b4/train/python
 
 from TestFunction import Test
 
-def likes(names):
-  if len(names) < 1: return "no one likes this"
-  if len(names) == 1: return f"{names[0]} likes this"
-  if len(names) == 2: return f"{names[0]} and {names[1]} like this"
-  if len(names) == 3: return f"{names[0]}, {names[1]} and {names[2]} like this"
-  return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+def even_or_odd(s):
+  odd = 0
+  even = 0
+  for digit in s:
+    if int(digit)%2 == 0: even += int(digit)
+    else: odd += int(digit)
+  if even > odd: return 'Even is greater than Odd'
+  if even < odd: return 'Odd is greater than Even'
+  return 'Even and Odd are the same'
 
 
 test = Test(None)
-test.it('Basic tests')
-test.assert_equals(likes([]), 'no one likes this')
-test.assert_equals(likes(['Peter']), 'Peter likes this')
-test.assert_equals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
-test.assert_equals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
-test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
+test.it("Sample tests")
+test.assert_equals(even_or_odd('12'), 'Even is greater than Odd')
+test.assert_equals(even_or_odd('123'), 'Odd is greater than Even')
+test.assert_equals(even_or_odd('112'), 'Even and Odd are the same')
