1e82c6286859b2fac13c4b454edbf0102dbb10a6
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-10-01 14:48:14 +0300

    :sparkles: added new kata

--- Python/[5 kyu] string incrementer.py
+++ Python/[5 kyu] string incrementer.py
@@ -16,6 +16,28 @@ def increment_string(s):
   return "".join([word, newIncrement])
 
 
+# def increment_string(strng):
+  
+#   # strip the decimals from the right
+#   stripped = strng.rstrip('1234567890')
+  
+#   # get the part of strng that was stripped
+#   ints = strng[len(stripped):]
+  
+#   if len(ints) == 0:
+#       return strng + '1'
+#   else:
+#       # find the length of ints
+#       length = len(ints)
+  
+#       # add 1 to ints
+#       new_ints = 1 + int(ints)
+  
+#       # pad new_ints with zeroes on the left
+#       new_ints = str(new_ints).zfill(length)
+  
+#       return stripped + new_ints
+
 
 from TestFunction import Test
 test = Test(None)   
