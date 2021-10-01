52587a95a3e4dd5b287af2f22718d7be7925d1b4
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-10-01 16:35:52 +0300

    :sparkles: added new kata

---
+++ Python/[5 kyu] A chain adding function.py
@@ -0,0 +1,16 @@
+# see https://www.codewars.com/kata/539a0e4d85e3425cb0000a88/train/python
+
+class Addition(int):
+  def __call__(self, value):
+    return Addition(self + value)
+
+def add(n):
+  return Addition(n)
+
+
+from TestFunction import Test
+test = Test(None)   
+
+test.assert_equals(add(1), 1)
+test.assert_equals(add(1)(2), 3)
+# test.assert_equals(add(1)(2)(3), 6)
